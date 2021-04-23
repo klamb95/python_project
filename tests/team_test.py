59bbd105cbdb65ad778a9f4d3e369d9b547ccdfe
klamb95 <klamb1995@gmail.com>
2021-04-23 16:31:59 +0100

    team has sponsor test working

--- tests/team_test.py
+++ tests/team_test.py
@@ -9,3 +9,6 @@ class TestTeam(unittest.TestCase):
     
     def test_team_has_name(self):
         self.assertEqual("Grange", self.team.name)
+
+    def test_team_has_sponsor(self):
+        self.assertEqual("Nike", self.team.sponsor)
