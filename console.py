dd6341a37270c2cd07be44618cf64a0a2eb6858e
klamb95 <klamb1995@gmail.com>
2021-04-23 17:11:39 +0100

    delete_all working

--- console.py
+++ console.py
@@ -3,6 +3,7 @@ import pdb
 from models.team import Team
 import repositories.team_repository as team_repository
 
+team_repository.delete_all()
 
 team_1 = Team("Grange", "Nike")
 team_repository.save(team_1)
