a0681093b7d9bc273598d66363050e21998e48e8
klamb95 <klamb1995@gmail.com>
2021-04-23 16:45:54 +0100

    game has team_1_score working

--- tests/game_test.py
+++ tests/game_test.py
@@ -22,4 +22,7 @@ class TestGame(unittest.TestCase):
     def test_game_has_team_2(self):
         self.assertEqual(2, self.game.team_2)
 
+    def test_game_has_team_1_score(self):
+        self.assertEqual(1, self.game.team_1_score)
+
     
