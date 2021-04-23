a2b89a8d73c46f4e540b2065c5ff1cc4d1f04640
klamb95 <klamb1995@gmail.com>
2021-04-23 18:32:05 +0100

    team update method working

--- console.py
+++ console.py
@@ -5,16 +5,20 @@ import repositories.team_repository as team_repository
 
 team_repository.delete_all()
 
-team_1 = Team("Grange", "Nike")
-team_repository.save(team_1)
-team_2 = Team("Inverleith", "Adidas")
-team_repository.save(team_2)
-team_3 = Team("Kelburn", "Puma")
-team_repository.save(team_3)
+team1 = Team("Grange", "Nike")
+team_repository.save(team1)
+team2 = Team("Inverleith", "Adidas")
+team_repository.save(team2)
+team3 = Team("Kelburn", "Puma")
+team_repository.save(team3)
 
 
 # team_repository.select(23)
 
-team_repository.select_all()
+# team_repository.select_all()
 
 # team_repository.delete(30)
+
+team3.name = "Grangess"
+team_repository.update(team3)
+
