d9541b8147748055b83ef15743f0323ce4392420
klamb95 <klamb1995@gmail.com>
2021-04-25 11:55:06 +0100

    fix 1 tried

--- controllers/teams_controller.py
+++ controllers/teams_controller.py
@@ -9,10 +9,19 @@ def teams():
     teams = team_repository.select_all()
     return render_template("teams/index.html", teams = teams)
 
+# @teams_blueprint.route("/teams/<id>")
+# def show_team(id):
+#     # players = team_repository.players(id)
+#     games = team_repository.games(id)
+#     team_1_name = team_repository.select(games.team_1)['name']
+#     team_2_name = team_repository.select(games.team_2)['name']
+#     games.team_1 = team_1_name
+#     games.team_2 = team_2_name
+#     return render_template("teams/show.html",games=games, team_1 = team_1, team_2 = team_2)
+
+
 @teams_blueprint.route("/teams/<id>")
 def show_team(id):
-    # players = team_repository.players(id)
     games = team_repository.games(id)
     team = team_repository.select(id)
-    return render_template("teams/show.html",games=games, team=team)
-
+    return render_template("teams/show.html", games=games, team=team)
